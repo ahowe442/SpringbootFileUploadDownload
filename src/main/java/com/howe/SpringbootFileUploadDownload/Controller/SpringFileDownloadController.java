050699cef93edb0278389e72cd21d3791558c426
Abby H <howe.abigail@gmail.com>
2020-05-05 19:28:56 -0500

    adds userview  (view user icon and function has a bug)

--- src/main/java/com/howe/SpringbootFileUploadDownload/Controller/SpringFileDownloadController.java
+++ src/main/java/com/howe/SpringbootFileUploadDownload/Controller/SpringFileDownloadController.java
@@ -1,4 +1,45 @@
 package com.howe.SpringbootFileUploadDownload.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+
+@Controller
 public class SpringFileDownloadController {
+
+    @Autowired private ServletContext context;
+
+    @GetMapping(value = "/downloadfile/{fileName}/{modifiedFileName}")
+    public void downloadfile(@PathVariable String fileName, @PathVariable String modifiedFileName, HttpServletResponse response){
+        String fullPath = context.getRealPath("/images"+File.separator+modifiedFileName);
+        File file = new File(fullPath);
+        final int BUFFER_SIZE = 4096;
+        if(file.exists()){
+            try {
+                FileInputStream inputStream = new FileInputStream(file);
+                String mimeTye = context.getMimeType(fullPath);
+                response.setContentType(mimeTye);
+                response.setHeader("Content-disposition", "attachment; filename="+fileName);
+                OutputStream outputStream = response.getOutputStream();
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int bytesRead = -1;
+                while ((bytesRead = inputStream.read(buffer))!= -1){
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                inputStream.close();
+                outputStream.close();
+
+            }catch (Exception e){
+                // TODO: handle exception
+            }
+        }
+    }
 }
